0d18b38a3c5607b81f07e8a9b4865e22a358b2bd
emre <emre@example.com>
2023-01-04 19:11:48 +0300

    dd

---
+++ Ucgen.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ucgen {
+    public static void main(String[] args) {
+        // üç kenar uzunluğu kullanıcıdan alınan üçgenin alanını hesaplama
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Üçgenin Alanı İçin 3 Kenarı Giriniz");
+        System.out.println("İlk Kenar Uzunluğunu Giriniz: ");
+        int a = scanner.nextInt();
+        System.out.println("İkinci Kenar Uzunluğunu Giriniz: ");
+        int b = scanner.nextInt();
+        System.out.println("Üçüncü Kenar Uzunluğunu Giriniz");
+        int c = scanner.nextInt();
+
+        int u = (a+b+c) / 2 ;
+        double alan = Math.sqrt( u* (u-a) * (u-b) * (u-c));
+
+        System.out.println("Üç uzunluğu kullanıcıdan alınmış üçgenin alanı: " + alan);
+    }
+}
