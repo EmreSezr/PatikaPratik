7124460c6c4fc3cff1a89e8fbdaa8bf709d0e464
emre <emre@example.com>
2023-01-04 19:23:21 +0300

    Java'da gidilen mesafeye göre taksimetre tutarını ekrana yazdıran program

---
+++ Taksimetre.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Taksimetre {
+    public static void main(String[] args) {
+        //Java'da gidilen mesafeye göre taksimetre tutarını ekrana yazdıran program
+        Scanner scanner = new Scanner(System.in);
+        double  mesafe, ucret ;
+        System.out.println("Kaç Kilometre Gidildiğini Yazınız: ");
+        mesafe = scanner.nextInt();
+        ucret = mesafe * 2.20 + 10 ;
+
+        if ( ucret <20 ) {
+            System.out.println("Taksi Ücreti 20 TL'dir.");
+        }else{
+            System.out.println("Taksi Ücreti " + ucret + "TL'dir.");
+        }
+
+    }
+}
