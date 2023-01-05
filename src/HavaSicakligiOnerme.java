7d3e5fd14f07e8405a63cb2746aba59870e6fdaa
emre <emre@example.com>
2023-01-05 22:47:22 +0300

    Not Ortalaması hesaplama programı

---
+++ src/HavaSicakligiOnerme.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HavaSicakligiOnerme {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hava kaç derece yazınız: ");
+        int weather = input.nextInt();
+
+        if (weather <= 5) {
+            System.out.println("Kayak yapabilirsiniz.");
+        } else if (weather > 5 && weather <= 15) {
+            System.out.println("Sinemaya gidebilirsiniz.");
+        } else if (weather > 15 && weather <= 25) {
+            System.out.println("Pikniğe gidebiliresiniz.");
+        } else if (weather > 25) {
+            System.out.println("Yüzmeye gidebilirsiniz. ");
+        }
+
+
+    }
+}
