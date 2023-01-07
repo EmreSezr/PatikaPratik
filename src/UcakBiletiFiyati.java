abba3e246f03676971eeebb96a6a6bf469a633fc
emre <emre@example.com>
2023-01-07 17:33:33 +0300

    Java ile mesafeye ve şartlara göre uçak bileti fiyatı hesaplayan program

--- src/UcakBiletiFiyati.java
+++ src/UcakBiletiFiyati.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class UcakBiletFiyatHesaplama {
+public class UcakBiletiFiyati {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
