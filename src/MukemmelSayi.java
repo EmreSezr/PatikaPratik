69ed4a65507cad7c84f1aac09d8326b3c24eff9a
EmreSezr <101684245+EmreSezr@users.noreply.github.com>
2023-01-08 14:21:37 +0300

    Klavyeden girilen bir sayının mükemmel sayı olup/olmadığını bulan program

---
+++ src/MukemmelSayi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MukemmelSayi {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.print("Bir sayı giriniz: ");
+int sayi = input.nextInt();
+int toplam = 0;
+for (int i = 1; i < sayi; i++) {
+if (sayi % i == 0) {
+toplam += i;
+}
+}
+if (toplam == sayi) {
+System.out.println("Mükemmel sayıdır.");
+} else {
+System.out.println("Mükemmel sayı değildir.");
+}
+}
+}
