08d5bfa11f16603335096efd52deb89b7af0f218
EmreSZR <emre_sezer1997@hotmail.com>
2023-01-08 16:23:05 +0300

    Yıldızlar ile ters üçgen yapma programı

---
+++ src/TersUcgen.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak Sayısını Giriniz: ");
+        int number = input.nextInt();
+
+        for (int i = number; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
