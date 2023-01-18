abf881b764229d4118180c4e00d2d93bb381056c
EmreSZR <emre_sezer1997@hotmail.com>
2023-01-18 10:58:31 +0300

    Java Sınıflar ile boks maçını simüle eden program

--- OOP/BoksGame/MainBoks.java
+++ OOP/BoksGame/MainBoks.java
@@ -1,5 +1,11 @@
 public class MainBoks {
     public static void main(String[] args) {
 
+        Fighter fighter1 = new Fighter("Khabib", 15, 100, 90, 0,0);
+        Fighter fighter2 = new Fighter("Connor", 10, 95, 100, 0,1);
+        Ring r = new Ring(fighter1, fighter2, 90, 100);
+        r.run();
     }
 }
+
+
