879ebcc647dd6e1185d5da381c6b1e927a333dad
EmreSZR <emre_sezer1997@hotmail.com>
2023-01-18 13:06:26 +0300

    Employee Salary Calculator

--- OOP/SalaryCalculator/Employee.java
+++ OOP/SalaryCalculator/Employee.java
@@ -1,15 +1,53 @@
-public class Employee {
-    String name;    //  Çalışanın adı ve soyadı
-    int salary;     //  Çalışanın maaşı
-    int workHours;  //  Haftalık çalışma saati
-    int hireYear;   //  İşe başlangıç yılı
 
-    public Employee(String name, int salary, int workHours, int hireYear) {
+class Employee {
+    private String name;
+    private double salary;
+    private int workHours;
+    private int hireYear;
+
+    public Employee(String name, double salary, int workHours, int hireYear) {
         this.name = name;
-        this.salary= salary;
+        this.salary = salary;
         this.workHours = workHours;
         this.hireYear = hireYear;
     }
 
+    public double tax() {
+        if (salary > 0 && salary <= 1000) {
+            return 0;
+        } else if (salary > 0 && salary > 1000) {
+            return salary = salary - (salary * 0.03);
+        } else {
+            System.out.println("Maaş Eksi Değer Alamaz");
+        }
+        return 0;
+    }
 
+    public double bonus() {
+        if (workHours > 40) {
+            return salary += (workHours - 40) * 30;
+        } else {
+            return salary += 0;
+        }
+    }
+
+    public double raiseSalary() {
+        int yearsOfService = 2023 - hireYear;
+
+        if (yearsOfService >= 0 && yearsOfService < 10) {
+            salary += salary * 0.05;
+        } else if (yearsOfService >= 10 && yearsOfService < 20) {
+            salary += salary * 0.10;
+        } else if (yearsOfService >= 20) {
+            salary += salary * 0.15;
+        }
+        return salary;
+    }
+
+    public String toString() {
+        return "NAME= " + name
+                + ", \nSALARY= " + salary
+                + ", \nWORK-HOURS= " + workHours
+                + ", \nHIRE-YEAR= " + hireYear + "\n===========================================\n ";
+    }
 }
--- OOP/SalaryCalculator/MainSalary.java
+++ OOP/SalaryCalculator/MainSalary.java
@@ -1,5 +1,12 @@
 public class MainSalary {
     public static void main(String[] args) {
-        System.out.println("deneme");
+        Employee employee = new Employee("John Doe", 990, 40, 2023);
+        System.out.println("\nÇalışan:\n=========================================== ");
+        System.out.println(employee.toString());
+        System.out.println("Vergi           : " + employee.tax());
+        System.out.println("Mesai+Ücret     : " + employee.bonus());
+        employee.raiseSalary();
+        System.out.println("\n     \nEkstra Ücretler İle Çalışan Maaşı:  \n===========================================" +
+                "\n"+employee.toString());
     }
 }
