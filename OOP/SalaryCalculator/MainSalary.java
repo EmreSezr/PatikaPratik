65f3d20b1598914c60da887f4861e4b24e1716f4
EmreSezr <101684245+EmreSezr@users.noreply.github.com>
2023-01-29 00:41:57 +0300

    Updated main

--- OOP/SalaryCalculator/MainSalary.java
+++ OOP/SalaryCalculator/MainSalary.java
@@ -1,6 +1,6 @@
 public class MainSalary {
     public static void main(String[] args) {
         Employee employee = new Employee("John Doe", 990, 40, 2023);
-        System.out.println(employee.toString());
+        System.out.println(employee.toPrint());
     }
 }
