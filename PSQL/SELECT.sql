1d285b271dd2fadde57976ec2c1f6d81079c9a99
EmreSZR <emre_sezer1997@hotmail.com>
2023-02-01 18:32:10 +0300

    SELECT PSQL

---
+++ PSQL/SELECT.sql
@@ -0,0 +1,7 @@
+-- SELECT İŞLEMLERİ
+
+-- film datasındaki bütün sütunları alırız
+SELECT * FROM film;
+
+-- actor datasındaki sadece isim ve soyisim kısmını aldık
+SELECT first_name, last_name FROM actor
